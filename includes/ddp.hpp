933c234b85ef533b3bf4679bcd6db85772f0a022
rob aux <robin.auxietre@epitech.eu>
2022-10-12 15:34:58 +0200

    [add] Iruler

--- includes/ddp.hpp
+++ includes/ddp.hpp
@@ -8,6 +8,23 @@
 #ifndef DDP_HPP_
 #define DDP_HPP_
 
+#include <iostream>
+#include <utility>
+#include <vector>
+
+enum prisonner_decision {
+    SILENCE,
+    BETRAY
+};
+
+
+class IRuler {
+    public:
+        virtual ~IRuler() = default;
+        virtual std::pair<int,int> calculate(prisonner_decision prisonner1_decision, prisonner_decision prisonner2_decision) = 0;
+    }
+
+
 class ddp {
     public:
         ddp();
